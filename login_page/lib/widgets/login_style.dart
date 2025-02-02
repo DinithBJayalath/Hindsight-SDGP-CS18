1f052fd086dc579beb0ac0a950dc4abc942ad050
Achintha Ekanyake <achiekanayake2@gmail.com>
2025-02-02 14:40:43 +0530

    custom widget for login adding background image

---
+++ login_page/lib/widgets/login_style.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class login_style extends StatelessWidget {
+  const login_style({super.key, this.child});
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/welcome_background.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          SafeArea(
+            child: child!,
+          ),
+        ],
+      ),
+    );
+  }
+}
