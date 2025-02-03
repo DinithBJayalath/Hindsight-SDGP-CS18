280f3464f687127e953aeb3e30b0a415e18ed2ba
Achintha Ekanyake <achiekanayake2@gmail.com>
2025-02-03 13:59:37 +0530

    Removed child! (forced null safety)

--- login_page/lib/widgets/login_style.dart
+++ login_page/lib/widgets/login_style.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class login_style extends StatelessWidget {
-  const login_style({super.key, this.child});
-  final Widget? child;
+  const login_style({super.key, required this.child});
+  final Widget child;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 248, 228, 252),
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
       body: Stack(
         children: [
           Container(
             color: const Color.fromARGB(255, 248, 228, 252),
           ),
           SafeArea(
-            child: child!,
+            child: child,
           ),
         ],
       ),
