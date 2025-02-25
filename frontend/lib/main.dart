e69b9732032df949be69cf0f81e5234c914a78bf
Achintha Ekanyake <achiekanayake2@gmail.com>
2025-02-25 20:08:14 +0530

    refactor main app structure to support authentication and dynamic home screen loading

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/home_screen.dart';
+import 'package:frontend/screens/login_screen.dart';
 import 'package:frontend/screens/welcome_screen.dart';
+import 'package:frontend/services/auth_service.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget _defaultHome = const WelcomeScreen(); // Default starting screen
+  final AuthService _authService = AuthService();
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthentication();
+  }
+
+  Future<void> _checkAuthentication() async {
+    setState(() {
+      _isLoading = true;
+    });
+    final isLoggedIn = !(await _authService.isTokenExpired());
+
+    if (isLoggedIn) {
+      _defaultHome = const HomeScreen(userInfo: {});
+    } else {
+      _defaultHome = const WelcomeScreen();
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Login Screen",
+      title: "Hindsight",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const WelcomeScreen(),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(userInfo: {}),
+      },
+      home: _isLoading
+          ? const Scaffold(
+              body: Center(
+                  child: CircularProgressIndicator())) // Loading indicator
+          : _defaultHome,
     );
   }
 }
