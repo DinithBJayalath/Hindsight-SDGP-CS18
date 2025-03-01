e255b7d7901b2162c5774662645da631ac263d2c
Ruwantha Kavishan <kavishan.20231108@iit.ac.lk>
2025-03-01 07:58:55 +0530

    Improve text formatting options in JournalWritingScreen

--- frontend/lib/screens/journal_writing_screen.dart
+++ frontend/lib/screens/journal_writing_screen.dart
@@ -22,6 +22,12 @@ class _JournalWritingScreenState extends State<JournalWritingScreen> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _entryController = TextEditingController();
 
+  bool isBold = false;
+  bool isItalic = false;
+  bool isUnderlined = false;
+  double fontSize = 16.0;
+  Color textColor = Colors.black;
+
   void _saveEntry() {
     if (_titleController.text.isNotEmpty && _entryController.text.isNotEmpty) {
       widget.addJournalEntry(
@@ -31,6 +37,36 @@ class _JournalWritingScreenState extends State<JournalWritingScreen> {
       );
       Navigator.pop(context);
     }
+  }
+
+  void _toggleBold() {
+    setState(() {
+      isBold = !isBold;
+    });
+  }
+
+  void _toggleItalic() {
+    setState(() {
+      isItalic = !isItalic;
+    });
+  }
+
+  void _toggleUnderline() {
+    setState(() {
+      isUnderlined = !isUnderlined;
+    });
+  }
+
+  void _changeFontSize(double size) {
+    setState(() {
+      fontSize = size;
+    });
+  }
+
+  void _changeTextColor(Color color) {
+    setState(() {
+      textColor = color;
+    });
   }
 
   @override
@@ -50,8 +86,7 @@ class _JournalWritingScreenState extends State<JournalWritingScreen> {
         ],
       ),
       body: Padding(
-        padding:
-            const EdgeInsets.all(20.0), // Increased padding for better layout
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
             TextField(
@@ -63,11 +98,82 @@ class _JournalWritingScreenState extends State<JournalWritingScreen> {
               ),
             ),
             SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.format_bold,
+                      color: isBold ? Colors.blue : Colors.black),
+                  onPressed: _toggleBold,
+                ),
+                IconButton(
+                  icon: Icon(Icons.format_italic,
+                      color: isItalic ? Colors.blue : Colors.black),
+                  onPressed: _toggleItalic,
+                ),
+                IconButton(
+                  icon: Icon(Icons.format_underline,
+                      color: isUnderlined ? Colors.blue : Colors.black),
+                  onPressed: _toggleUnderline,
+                ),
+                IconButton(
+                  icon: Icon(Icons.color_lens, color: textColor),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text("Select Text Color"),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ColorPickerButton(
+                              color: Colors.black,
+                              onPressed: () => _changeTextColor(Colors.black),
+                            ),
+                            ColorPickerButton(
+                              color: Colors.blue,
+                              onPressed: () => _changeTextColor(Colors.blue),
+                            ),
+                            ColorPickerButton(
+                              color: Colors.red,
+                              onPressed: () => _changeTextColor(Colors.red),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                DropdownButton<double>(
+                  value: fontSize,
+                  items: [14.0, 16.0, 18.0, 20.0, 24.0].map((size) {
+                    return DropdownMenuItem(
+                      value: size,
+                      child: Text(size.toInt().toString(),
+                          style: TextStyle(fontSize: size)),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    if (value != null) _changeFontSize(value);
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
             Expanded(
               child: TextField(
                 controller: _entryController,
                 maxLines: null,
                 keyboardType: TextInputType.multiline,
+                style: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+                  fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+                  decoration: isUnderlined
+                      ? TextDecoration.underline
+                      : TextDecoration.none,
+                  color: textColor,
+                ),
                 decoration: InputDecoration(
                   hintText: "Write your thoughts...",
                   border: OutlineInputBorder(
@@ -93,6 +199,25 @@ class _JournalWritingScreenState extends State<JournalWritingScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class ColorPickerButton extends StatelessWidget {
+  final Color color;
+  final VoidCallback onPressed;
+
+  const ColorPickerButton(
+      {super.key, required this.color, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.circle, color: color, size: 30),
+      onPressed: () {
+        onPressed();
+        Navigator.pop(context);
+      },
     );
   }
 }
