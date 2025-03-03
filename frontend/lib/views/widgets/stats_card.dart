50e20a8dc83f50dd05db85300f2e44b0ab9eaee8
Kavixr <kavindurajapaksha848@gmail.com>
2025-03-04 04:19:28 +0530

    Added StatsCard widget with basic structure.

---
+++ frontend/lib/views/widgets/stats_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatsCard extends StatelessWidget {
+  final Map<String, int> stats;
+
+  const StatsCard({Key? key, required this.stats}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
