9ee6a598fd9a704b488f9cdd021bbbe76d045e5d
DinithBJayalath <dinithbjayalath@gmail.com>
2025-03-22 21:42:45 +0530

    Added health checks to debug deployment

--- algorithms/algorithms_service.py
+++ algorithms/algorithms_service.py
@@ -1,4 +1,5 @@
 import grpc
+import os
 from concurrent import futures
 import algorithms_pb2 as algorithms_pb2
 import algorithms_pb2_grpc as algorithms_pb2_grpc
@@ -14,11 +15,12 @@ class JournalAnalyzer(algorithms_pb2_grpc.JournalAnalyzerServicer):
         return algorithms_pb2.AnalyzeResponse(response = emotion)
 
 def serve():
+    port = int(os.environ.get('PORT', '8080'))
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     algorithms_pb2_grpc.add_JournalAnalyzerServicer_to_server(JournalAnalyzer(), server)
-    server.add_insecure_port("[::]:8080")
+    server.add_insecure_port(f"[::]:{port}")
     server.start()
-    print("Server started at port 8080")
+    print(f"Server started at port {port}")
     server.wait_for_termination()
 
 if __name__ == "__main__":
