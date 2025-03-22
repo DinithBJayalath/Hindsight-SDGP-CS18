8a56b18660301bdbd452128f021e72a60de249ea
Suhas Jayalath <164993003+swasjay@users.noreply.github.com>
2025-03-22 16:04:36 +0530

    Added letters module with controller, service, and DTO for managing letters.

---
+++ backend-core/src/letters/letters.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { LettersController } from "./letters.controller";
+import { LettersService } from "./letters.service";
+import { Letter, LetterSchema } from "./schemas/letter.schema";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Letter.name, schema: LetterSchema }]),
+  ],
+  controllers: [LettersController],
+  providers: [LettersService],
+})
+export class LettersModule {}
