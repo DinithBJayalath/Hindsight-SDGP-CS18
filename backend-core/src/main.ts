7385cedaca116098abe7930915b4f1817e1bee47
DinithBJayalath <dinithbjayalath@gmail.com>
2025-03-24 06:40:18 +0530

    configuring backend for cloud deployment

--- backend-core/src/main.ts
+++ backend-core/src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
   }));
   
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
 }
 bootstrap();
