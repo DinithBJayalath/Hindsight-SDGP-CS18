d204f37235170af4ed9ed48e7a0f09e54e874ceb
Savindu <savindugimhan@gmail.com>
2025-04-22 21:32:53 +0530

    Added functionality to save to database

--- backend-core/src/moodcheck/moodcheck.controller.ts
+++ backend-core/src/moodcheck/moodcheck.controller.ts
@@ -1,18 +1,24 @@
 import { Body, Controller,Get,Post} from '@nestjs/common';
 import { MoodCheckDto } from './moodcheck.dto';
+import { plainToInstance } from 'class-transformer';
+import { MoodcheckService } from './moodcheck.service';
 
 @Controller('moodcheck')
 export class MoodcheckController {
+    constructor(private moodcheckService: MoodcheckService) {}
     @Get()
     moodCheck(){
         return "Hello world";
 
     }
     @Post()
-    saveMood(@Body() data:string){ // Using MoodCheckDto here causes an error
-        console.log(data)
-        return data;
+    saveMood(@Body() body: { profileId: string; data: MoodCheckDto }) {
+        const { profileId, data } = body;
+        const moodCheckDto = plainToInstance(MoodCheckDto, data);
+        this.moodcheckService.saveMood(profileId, moodCheckDto);
+        return data; // Returning the data again for debugging perposes
     }
+
 
 
     
